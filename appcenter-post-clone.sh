e122b14fd2c10062304eca93efd3448496f1d51f
Wojciech Nadurski <wnadurski@atlassian.com>
2018-06-04 21:32:08 +0200

    cat appcenter config

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -8,6 +8,7 @@ SOURCE=`dirname $0`
 if [[ ! -z "${API_KEY}" ]]; then
     echo "Injecting APPCENTER_API_KEY"
     echo "{ \"app_secret\": \"$API_KEY\" }" > $SOURCE/android/app/src/main/assets/appcenter-config.json
+    cat $SOURCE/android/app/src/main/assets/appcenter-config.json
 fi
 
 # Set up development URL for expo
