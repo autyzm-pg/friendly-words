23ce9a21eae1ed39823704332657858a323dd7b4
anntr <a.trapczynska@gmail.com>
2018-06-10 21:08:05 +0200

    quickfix too big command text

--- app/components/ui/Header.js
+++ app/components/ui/Header.js
@@ -2,4 +2,4 @@ import React from "react";
 import CapriolaText from "./CapriolaText";
 import glamorous from "glamorous-native";
 
-export const Header = glamorous(CapriolaText)({fontSize: 60});
+export const Header = glamorous(CapriolaText)({fontSize: 50 });
