689d86ff10a13d3a0dbf755da190d570b8ad928d
Wojciech Nadurski <wnadurski@atlassian.com>
2018-06-10 21:15:59 +0200

    Expo svg to react-native-svg

--- android/app/src/main/java/com/friendlywords/MainApplication.java
+++ android/app/src/main/java/com/friendlywords/MainApplication.java
@@ -3,6 +3,7 @@ package com.friendlywords;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import net.no_mad.tts.TextToSpeechPackage;
 import com.rnfs.RNFSPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new VectorIconsPackage(),
             new TextToSpeechPackage(),
             new RNFSPackage(),
--- app/svg/balloons.js
+++ app/svg/balloons.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Svg} from "expo";
+import {Svg} from "react-native-svg";
 const {Path, Polygon, G} = Svg;
 
 //Icons made by Freepik from https://www.flaticon.com is licensed by http://creativecommons.org/licenses/by/3.0/ Creative Commons BY 3.0
