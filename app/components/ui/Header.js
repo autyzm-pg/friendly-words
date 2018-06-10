0cca91896d36dff4696d7ff45fff04fe36a84c23
anntr <a.trapczynska@gmail.com>
2018-06-10 20:57:00 +0200

    maybe this will help

--- app/components/ui/Header.js
+++ app/components/ui/Header.js
@@ -2,4 +2,4 @@ import React from "react";
 import CapriolaText from "./CapriolaText";
 import glamorous from "glamorous-native";
 
-export const Header = glamorous(CapriolaText)({fontSize: 64});
+export const Header = glamorous(CapriolaText)({fontSize: 60});
