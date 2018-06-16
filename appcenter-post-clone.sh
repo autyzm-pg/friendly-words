c4047a7426e86aacd9ce93f984d43b62725d1ad2
Wojciech Nadurski <wnadurski@atlassian.com>
2018-06-16 12:46:39 +0200

    Using sed instead of echo to inject appcenter's api_key

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,16 +5,7 @@ echo "Starting post clone script..."
 SOURCE=`dirname $0`
 
 # Add ms appcenter api key
-if [[ ! -z "${API_KEY}" ]]; then
-    echo "Injecting APPCENTER_API_KEY to $SOURCE/android/app/src/main/assets/appcenter-config.json"
-    echo "{ \"app_secret\": \"$API_KEY\" }" > $SOURCE/android/app/src/main/assets/appcenter-config.json
-    echo "{ \"app_secret\": \"$API_KEY\" }" > $SOURCE/android/app/build/intermediates/assets/dev19/debug/appcenter-config.json
-fi
-
-# Set up development URL for expo
-if [[ ! -z "${EXPO_DEV_URL}" ]]; then
-    echo "Setting development host: $EXPO_DEV_URL"
-    sed -i -e "s@://.*:@://$EXPO_DEV_URL:@g" $SOURCE/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
-fi
+sed -i .bak -e "s/{APPCENTER_API_KEY}/$APPCENTER_API_KEY/g" $SOURCE/android/app/src/main/assets/appcenter-config.json
+cat $SOURCE/android/app/src/main/assets/appcenter-config.json
 
 echo "Post clone script finished."
