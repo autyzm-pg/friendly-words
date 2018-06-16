a8c411479869396dcce90691a41f98404b6bbfdf
Wojciech Nadurski <wnadurski@atlassian.com>
2018-06-16 12:36:08 +0200

    Squashed commit of the following:
    commit 6430fc33d537cc0770ddea3770f0e74ad887e2ac
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sat Jun 16 12:32:48 2018 +0200
    
        Fixing changes after removing expo
    
    commit 689d86ff10a13d3a0dbf755da190d570b8ad928d
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 21:15:59 2018 +0200
    
        Expo svg to react-native-svg
    
    commit 8b158758bf53e7cc23a5a4d8e0df222606e4f3ba
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:57:35 2018 +0200
    
        Added permission
    
    commit ddc2a0397f80f7f3cb82cca6e8b4d1ee121d5428
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:54:17 2018 +0200
    
        Removed expo dependecny
    
    commit 2ce86cb832374a33497b56425a9e33a05e71bef7
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:36:35 2018 +0200
    
        Unremoved files
    
    commit fbd7cb0d79a519d6d3c720518ec77cdb38fb7e1f
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:31:10 2018 +0200
    
        Installing packages
    
    commit 4afe0b98134fe56349308afb4e53eae4d3dad701
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:24:12 2018 +0200
    
        Fresh react native project
    
    commit da4b2be7f12bbd85c1f755bf26be5e26abf6125b
    Author: Wojciech Nadurski <wnadurski@atlassian.com>
    Date:   Sun Jun 10 20:17:04 2018 +0200
    
        Delete everything

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expd16bca44a7e84f759fcce334a17cc6ea://192.168.43.147:19000";
-
-}
