fcc5ac1d741a82f23268757a1c2ca20f025265b5
adzka95 <ada.rynkowska@gmail.com>
2018-10-04 20:16:18 +0200

    change cars color

--- app/components/animations/AnimatedCars.js
+++ app/components/animations/AnimatedCars.js
@@ -22,15 +22,15 @@ export default class CarsAnimated extends Component {
                 styles: [
                     {carColor: "#1983db", linesColor: "#0f53b7"},
                     {carColor: "#CC0099", linesColor: "#99147D"},
-                    {carColor: "#15E6CD", linesColor: "#1CCAD8"},
-                    {}]
+                    {carColor: "#91E086", linesColor: "#87CC7E"}
+                    ]
             },
             {
                 component: AnimatedBusCar,
                 styles: [
-                    {carColor: "#FF9914", linesColor:"#000FF0"},
-                    {carColor: "#CFFFB3", linesColor:"#FF0000"},
-                    {carColor: "#F7EF66", linesColor:"#00FF00"}
+                    {carColor: "#15E6CD", linesColor: "#1CCAD8"},
+                    {carColor: "#FF9914", linesColor:"#D87805"},
+                    {carColor: "#CC0099", linesColor: "#99147D"}
                 ]
             },
             {
@@ -46,39 +46,48 @@ export default class CarsAnimated extends Component {
                 styles: [
                     {carColor: "#47D2A5", darkerCarColor: "#35CA99"},
                     {carColor: "#EF5562", darkerCarColor: "#DB3D45"},
-                    {carColor: "#30BCE9", darkerCarColor: "#29A7D3"}
+                    {carColor: "#30BCE9", darkerCarColor: "#29A7D3"},
+                    {carColor: "#FF9914", darkerCarColor: "#D87805"},
+                    {carColor: "#F7EF66", darkerCarColor: "#ecc142"},
                 ]
-            },
+            }
+            ,
             {
                 component:AnimatedGarbageTruckCar,
                 styles: [
                     {carColor: "#47D2A5", darkerCarColor: "#35CA99",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"},
+                        containerColor:"#DCDBDE", containerLinesColor :"#CAC9CD"},
                     {carColor: "#EF5562", darkerCarColor: "#DB3D45",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"},
+                        containerColor:"#CAC9CD", containerLinesColor :"#736F7A"},
                     {carColor: "#30BCE9", darkerCarColor: "#29A7D3",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"}
+                        containerColor:"#DCDBDE", containerLinesColor :"#CAC9CD"}
                 ]
             },
             {
                 component:AnimatedTramCar,
                 styles: [
-                    {carColor: "#47D2A5", linesColor : "#30BCE9", darkerCarColor: "#35CA99"},
-                    {carColor: "#EF5562", linesColor : "#30BCE9", darkerCarColor: "#DB3D45"},
-                    {carColor: "#30BCE9", linesColor : "#30BCE9", darkerCarColor: "#29A7D3"}
+                    {carColor: "#47D2A5", linesColor : "#279772",
+                        darkerCarColor: "#32BF91"},
+                    {carColor: "#EF5562", linesColor : "#FFAB3E",
+                        darkerCarColor: "#DB3D45"},
+                    {carColor: "#30BCE9", linesColor : "#2083A7",
+                        darkerCarColor: "#29A7D3"}
                 ]
             },
             {
                 component:AnimatedTruckCar,
                 styles: [
-                    {carColor: "#47D2A5", darkerCarColor: "#35CA99",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"},
+                    {carColor: "#CC0099", darkerCarColor: "#99147D",
+                        containerColor:"#DCDBDE", containerLinesColor :"#CAC9CD"},
                     {carColor: "#EF5562", darkerCarColor: "#DB3D45",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"},
+                        containerColor:"#CAC9CD", containerLinesColor :"#736F7A"},
+                    {carColor: "#F7EF66", darkerCarColor: "#ecc142",
+                        containerColor:"#CAC9CD", containerLinesColor :"#736F7A"},
                     {carColor: "#30BCE9", darkerCarColor: "#29A7D3",
-                        containerColor:"#000000", containerLinesColor :"#F00F0F"}
+                        containerColor:"#DCDBDE", containerLinesColor :"#CAC9CD"}
                 ]
-            }]
+            }
+        ]
     }
 
     render() {
@@ -87,9 +96,7 @@ export default class CarsAnimated extends Component {
                 let car = _.sample(this.cars);
                 let RandomCar = car.component;
                 let carStyles = _.sample(car.styles);
-                //let randomSize = _.random(130, 180);
-                let randomSize = _.random(300, 400);
-
+                let randomSize = _.random(130, 180);
                 return <RandomCar {...carStyles}{...{width: randomSize, height:randomSize}} key={idx}/>
             })
             }
