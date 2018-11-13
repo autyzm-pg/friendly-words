614ba73891b10feee615ee0cc10fa3831412fcab
Ada Rynkowska <ada.rynkowska@gmail.com>
2018-11-13 16:04:16 +0100

    add current configuration name to main screen

--- app/containers/MainScreen.js
+++ app/containers/MainScreen.js
@@ -2,13 +2,22 @@ import React from 'react';
 import {Button, StatusBar} from 'react-native';
 import {Layout} from "./Game";
 import {PlayButton} from '../components/ui/borderedButton/BorderedButton';
-import {JumboHeader} from "../components/ui/Header";
+import {JumboHeader, Configurationheader} from "../components/ui/Header";
 import {View} from "glamorous-native";
+import ConfigConsumer from "./ConfigConsumer";
+import Game from "./Game";
 
 export default MainScreen = ({navigation}) =>
 	<Layout>
 		<View flex={1} alignItems={"center"} justifyContent={"center"}>
         	<JumboHeader>Przyjazne Słowa</JumboHeader>
+            <Configurationheader>Obecna konfiguracja:</Configurationheader>
+            <ConfigConsumer>
+                {(config) => {
+                    return <Configurationheader>{config.name}</Configurationheader>
+                	}
+                }
+            </ConfigConsumer>
 			<PlayButton onPress={() => navigation.navigate("Game")}/>
 		</View>
 	</Layout>
