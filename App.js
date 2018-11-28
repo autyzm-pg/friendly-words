0370d16268499b65999aca46ff943e811de2e6c1
Wojciech <w.nadurski@gmail.com>
2018-11-28 21:24:43 +0100

    Adding ask for permission

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from 'react'
 import Game from "./app/containers/Game"
-import {StackNavigator} from "react-navigation";
-import {Text, View} from "react-native";
-import _ from "lodash";
+import {StackNavigator} from "react-navigation"
+import {Text, View, PermissionsAndroid} from "react-native"
+import _ from "lodash"
 import {AdamConfig, defaultConfig, FrycekConfig, FrycekTestConfig, KacperConfig} from "./TEMP_CONFIGS"
-import MainScreen from "./app/containers/MainScreen";
-import {readActiveConfig, readConfigs} from "./app/services/db/configs";
-import {ModeTypes} from "./app/services/db/format";
-import ConfigProvider from "./app/containers/ConfigProvider";
-import ConfigConsumer from "./app/containers/ConfigConsumer";
+import MainScreen from "./app/containers/MainScreen"
+import {readActiveConfig, readConfigs} from "./app/services/db/configs"
+import {ModeTypes} from "./app/services/db/format"
+import ConfigProvider from "./app/containers/ConfigProvider"
+import ConfigConsumer from "./app/containers/ConfigConsumer"
 import Analytics from "appcenter-analytics"
-import SplashScreen from "./app/containers/SplashScreen";
+import SplashScreen from "./app/containers/SplashScreen"
+import ErrorScreen from "./app/containers/ErrorScreen"
 
 const configFromDbToGameScreen = (config) => ({
     ...config,
@@ -21,7 +22,7 @@ const configFromDbToGameScreen = (config) => ({
         isInTestMode: material.isInTestMode,
         isInLearningMode: material.isInLearningMode,
     })),
-});
+})
 
 function prepareLevels(materials, repetitions, optionsNumber) {
     let levels = _.shuffle(_.flatMap(materials, material =>
@@ -30,10 +31,10 @@ function prepareLevels(materials, repetitions, optionsNumber) {
                 name: material.name,
                 images: material.images,
                 isCorrectAnswer: true
-            })))));
+            })))))
 
-    levels = _.map(levels, level => _.map(level, material => ({...material, image: _.sample(material.images)})));
-    return _(levels);
+    levels = _.map(levels, level => _.map(level, material => ({...material, image: _.sample(material.images)})))
+    return _(levels)
 }
 
 const GameScreen = ({navigation}) => {
@@ -43,7 +44,7 @@ const GameScreen = ({navigation}) => {
                 const isTestMode = mode === ModeTypes.test
 
                 const materials = isTestMode ? _.filter(config.materials, 'isInTestMode') : config.materials,
-                    repetitions = isTestMode ? config.testConfig.numberOfRepetitions : config.numberOfRepetitions;
+                    repetitions = isTestMode ? config.testConfig.numberOfRepetitions : config.numberOfRepetitions
 
                 return <Game
                     levels={prepareLevels(materials, repetitions, config.picturesNumber)}
@@ -61,7 +62,7 @@ const GameScreen = ({navigation}) => {
             }
         </ConfigConsumer>
     )
-};
+}
 
 
 const AppNavigator = StackNavigator(
@@ -73,13 +74,13 @@ const AppNavigator = StackNavigator(
     {
         headerMode: "none",
         initialRouteName: "Splash"
-    });
+    })
 
 
 export default class App extends React.Component {
     state = {
         fontLoaded: false,
-    };
+    }
 
     async loadConfig() {
         const {id, mode = ModeTypes.learning} = await readActiveConfig()
@@ -87,7 +88,7 @@ export default class App extends React.Component {
 
         const configs = await readConfigs()
 
-        const activeConfig = configs.find(config => config.id === id) || defaultConfig;
+        const activeConfig = configs.find(config => config.id === id) || defaultConfig
 
         console.log("Active config:", activeConfig)
         return {
@@ -103,23 +104,54 @@ export default class App extends React.Component {
         console.log("Loaded all assets")
     }
 
-    async componentDidMount() {
-        Analytics.trackEvent("App loaded");
+    async requestExternalStoragePermission() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                {
+                    'title': 'Przyjazne Słowa - uprawnienia do zapisu',
+                    'message': "Aplikacja Przyjazne Słowa wymaga uprawnień do zapisu danych w pamięci urządzenia, aby móc komunikować się z aplikacją Przyjazne Słowa - Menadżer"
+                }
+            )
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log("You can use the camera")
+                return true;
+            } else {
+                console.log("Camera permission denied")
+                return false;
+            }
+        } catch (err) {
+            console.warn(err)
+            return false;
+        }
+    }
 
-        await this.loadAssets();
-        const {config, mode} = await this.loadConfig();
+    async componentDidMount() {
+        Analytics.trackEvent("App loaded")
+        const hasPermissions = await this.requestExternalStoragePermission();
+
+        if(!hasPermissions) {
+            this.setState({noPermissions: true})
+            return;
+        }
+
+        await this.loadAssets()
+        const {config, mode} = await this.loadConfig()
         this.setState({
             assetsLoaded: true,
             config: configFromDbToGameScreen(config),
             mode
-        });
+        })
     }
 
     render() {
+        if(this.state.noPermissions) {
+            return <ErrorScreen/>
+        }
         return !this.state.assetsLoaded ? <Text>Loading</Text> : (
             <ConfigProvider config={this.state.config} mode={this.state.mode}>
                 <AppNavigator/>
             </ConfigProvider>
-        );
+        )
     }
 }
